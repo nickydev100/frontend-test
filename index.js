fedaf1513b084c0f66c75fe813e16f76d959f1cf
Tony <jianjiang532@gmail.com>
2021-09-30 19:25:17 -0400

    done first step

---
+++ index.js
@@ -0,0 +1,77 @@
+var arr = [
+  {
+    'guest_type': 'crew',
+    'first_name': 'Marco',
+    'last_name': 'Burns',
+    'guest_booking': {
+        'room_no': 'A0073',
+        'some_array': [7,2,4]
+    },
+  },
+  {
+    'guest_type': 'guest',
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'guest_booking': {
+        'room_no': 'C73',
+        'some_array': [1,3,5,2,4,3]
+    },
+  },
+  {
+    'guest_type': 'guest',
+    'first_name': 'Jane',
+    'last_name': 'Doe',
+    'guest_booking': {
+        'room_no': 'C73',
+        'some_array': [1,3,5,2,4,3]
+    },
+  },
+  {
+    'guest_type': 'guest',
+    'first_name': 'Albert',
+    'last_name': 'Einstein',
+    'guest_booking': {
+        'room_no': 'B15',
+        'some_array': [2,5,6,3]
+    },
+  },
+  {
+    'guest_type': 'crew',
+    'first_name': 'Jack',
+    'last_name': 'Daniels',
+    'guest_booking': {
+        'room_no': 'B15',
+        'some_array': [2,5,6,3]
+    },
+  },
+  {
+    'guest_type': 'guest',
+    'first_name': 'Alan',
+    'last_name': 'Turing',
+    'guest_booking': {
+        'room_no': 'B15',
+        'some_array': [2,5,6,3]
+    },
+  },
+];
+
+function mutateArray(a) {
+  const ret = []
+  a.forEach(element => {
+    ret.push({
+      // ...element, // removed because guest_booking should be removed
+      guest_type: element.guest_type,
+      first_name: element.first_name,
+      last_name: element.last_name,
+      room_no: element.guest_booking.room_no,
+      some_array: element.guest_booking.some_array
+    })
+  });
+
+  return ret;
+}
+
+$(document).ready(function() {
+    $('#originalArray').html(JSON.stringify(arr, null, 2));
+    $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
+});
